379152a8c2fb15b4d87476dd3ee6e3a2f6b8c255
dhiren-rajesh <dhirenrajeshk@gmail.com>
2026-01-18 21:19:16 +0530

    Created an Fetch API tool

---
+++ src/api.py
@@ -0,0 +1,21 @@
+import requests
+from langchain_core.tools import tool
+
+BASE_URL= "https://ergast.com/api/f1"
+
+@tool
+def ergast_f1_api(endpoint: str) -> dict:
+    """
+    Fetch Formula 1 data from Ergast API
+
+    The enpoint must be a valid Ergast path:
+    - current/driverStandings
+    - past/driverStandings
+    - 2008/japan/results
+    - drivers/verstappen
+    """
+
+    url = f"{BASE_URL}/{endpoint}.json"
+    response = requests.get(url, timeout=10)
+    response.raise_for_status()
+    return response.json()
